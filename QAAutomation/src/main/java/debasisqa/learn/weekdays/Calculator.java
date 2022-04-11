b408de4f43261c71d232b86b1ad86f02807ff2f3
dev5680 <dev5680@gmail.com>
2022-04-11 13:37:59 -0400

    first commit

---
+++ QAAutomation/src/main/java/debasisqa/learn/weekdays/Calculator.java
@@ -0,0 +1,14 @@
+package debasisqa.learn.weekdays;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        int total = a + b;
+        return total;
+    }
+
+    public int sub(int a, int b){
+        int total = a - b;
+        return total;
+    }
+}
