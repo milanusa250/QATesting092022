9cd23b8849781f8ac46e00e1668efa7c2ce5f183
dev5680 <dev5680@gmail.com>
2022-10-12 17:34:10 -0400

    initial commit

---
+++ QATesting092022/src/main/java/org/rtscorp/animal/Mamal.java
@@ -0,0 +1,15 @@
+package org.rtscorp.animal;
+
+public class Mamal
+{
+    /** this program is for printing hello world
+    this is my first program*/
+
+    int numberOfHand=2;
+    int numberOfEyes=2;
+
+    public void response(){
+        System.out.print("This is a man and has "+ numberOfHand +" hands");
+    }
+
+}
